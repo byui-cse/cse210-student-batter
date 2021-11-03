1a80d53ab35c82eee41ab89a73ff883aa72b9a9c
Xander Newlun <xdnewlun1@byui.edu>
2021-11-03 13:18:08 -0600

    handle collision test

--- batter/game/handle_collisions_action.py
+++ batter/game/handle_collisions_action.py
@@ -18,9 +18,7 @@ class HandleCollisionsAction(Action):
         """
         bricks = cast["brick"] # there's only one
         paddle = cast["paddle"][0] # there's only one
-        bricks = cast["bricks"]
-        marquee.set_text("")
+        bricks = cast["brick"]
         for brick in bricks:
             if paddle.get_position().equals(bricks.get_position()):
-                description = bricks.get_description()
-                marquee.set_text(description) 
+                print("HIT")
