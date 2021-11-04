c3ac2ba3d943a9dffda44be11ced3d75b3a020af
brynlol <brynlol12@gmail.com>
2021-11-04 11:49:00 -0600

    added files and copied relevant code from rfk game, edited some of the code, left some todo

---
+++ batter/game/draw_actors_action.py
@@ -0,0 +1,40 @@
+from game.action import Action
+
+class DrawActorsAction(Action):
+    """ A code template for drawing actors. 
+        The responsibility of this class of objects is to draw to the screen
+        
+    Stereotype: 
+        Controller
+        
+    Attributes:
+        _output_service: an instance of Output_Service
+    """
+    def __init__(self, output_service):
+        """Class Contructor.
+        
+        Args:
+            self (DrawActorsAction): An instance of DrawActorsAction
+            output_service (OutputService): An instance of OutputService
+    """
+        self._output_service = output_service
+
+    def execute(self, cast):
+        #TODO: 
+        """Executes the action using the given actors.
+
+        Args:
+            cast (dict): The game actors {key: tag, value: list}.
+        """ 
+               
+        
+"""Example from RFK:
+        marquee = cast["marquee"][0] # there's only one
+        robot = cast["robot"][0] # there's only one
+        artifacts = cast["artifact"]        
+        self._output_service.clear_screen()
+        self._output_service.draw_actor(marquee)
+        self._output_service.draw_actors(artifacts)
+        self._output_service.draw_actor(robot)
+        self._output_service.flush_buffer()
+        """
