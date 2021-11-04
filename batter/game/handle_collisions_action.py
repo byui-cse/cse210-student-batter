7bea02ed211d0d66f7753113fa3323563a703fbc
awarbler <awoodfor@byui.edu>
2021-11-04 15:38:38 -0500

    updated handle

--- batter/game/handle_collisions_action.py
+++ batter/game/handle_collisions_action.py
@@ -17,18 +17,61 @@ class HandleCollisionsAction(Action):
         Args:
             cast (dict): The game actors {key: tag, value: list}.
         """
+        self.ball_brick_collision(cast)
+        # self.ball.paddle_constraints(cast)
+        # self.ball__handle_collision(cast)
+        # self.ball_ceiling_collision(cast)
+        # self.ball_floor_collision(cast)
+
+    def ball_brick_collision(self,cast):
         bricks = cast["brick"] # there's only one
         paddle = cast["paddle"][0] # there's only one
         ball = cast["ball"][0]
         for brick in bricks:
             if ball.get_position().equals(brick.get_position()):
-                brick.set_text("")
-                ball.set_velocity(Point(-1, 1))
+                ball.get_velocity().reverse_y()
+                ball.set_velocity(Point(1,- 1))
+                #brick.set_text("")
+                cast["brick"].remove(brick)
 
-        balPos = ball.get_position()
-        if balPos.get_x() == constants.MAX_X or balPos.get_x() == 0 or balPos.get_y() == constants.MAX_Y:
-            pass
+    def paddle_constraints(self, paddle):
+        pass
+    # Needs to get the position of the paddle.
+    # Compare paddle to 0.
+    # less then zero set paddle start back to zero
+    # If the paddle became more then 69 then reset back to 69
 
-        if balPos.get_y() == 0:
-            quit()
+    def ball_paddle_collision(self, paddle, ball):
+        pass
+        # Needs to loop through the length of the paddle and compare its position with the position of the ball.
+        # If the ball postition is equal to any portion of the paddle, bounce.
+        # need to return 
+
+
+    def ball_handle_constraints(self, ball):
+        pass
+        # Needs to find the position of the ball.
+        # Then grab the y value of the ball.
+        # The check to see if y equals 0 + 1
+        # if yes, bounce the ball.
+        # Find the x value of ball.
+        # if x equal 0 or Max_X bounce.
+
+    
+    def ball_floor_collision(self,cast):
+        # balPos = cast["ball"].get_position()
+        # if balPos.get_x() == constants.MAX_X or balPos.get_x() == 0 or balPos.get_y() == constants.MAX_Y:
+        #     pass
+
+        # if balPos.get_y() == 0:
+        #     quit()
+        # pass
+        # Needs to find the position of the ball.
+        # Then grab the y value of the ball.
+        # The check to see if y equals of MAX_Y
+        # Exit the system
+        pass
+    
+    def ball_ceiling_collision(self, ball):
+        pass
 
