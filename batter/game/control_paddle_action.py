4cf1ee619c886a6d8050428a6458869d799cbdeb
j-rubio <esc18010@byui.edu>
2021-11-05 00:10:43 -0600

    batter's inertia solved.

--- batter/game/control_paddle_action.py
+++ batter/game/control_paddle_action.py
@@ -34,7 +34,14 @@ class ControlPaddleAction(Action):
         direction = self._input_service.get_direction()
         x = direction.get_x() * constants.PADDLE_SPEED_FACTOR
         y = direction.get_y()
-              
+
+        # if x <= 1:
+        #     x = 1
+        # elif x + constants.PADDLE_LENGTH > constants.MAX_X - 1:
+        #     x = constants.MAX_X - constants.PADDLE_LENGTH - 1
+        #else:
+            #x *= constants.PADDLE_SPEED_FACTOR
+                          
         direction = Point(x, y)
         paddle = cast["paddle"][0]
         paddle.set_velocity(direction)
