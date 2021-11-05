a4888a96218b3ba0ed642390499974df58ed6f62
Jennifer Clark <jennifer.c.schultz@gmail.com>
2021-11-05 12:31:18 -0600

    add constants.py from rfk.

---
+++ batter/game/constants.py
@@ -0,0 +1,10 @@
+#start code from rfk  this one has to change because the  x and y for the screen
+#need to change. 
+import os
+
+MAX_X = 80
+MAX_Y = 20
+FRAME_LENGTH = 0.1
+PATH = os.path.dirname(os.path.abspath(__file__))
+MESSAGES = open(PATH + "/messages.txt").read().splitlines()
+ARTIFACTS = 30
