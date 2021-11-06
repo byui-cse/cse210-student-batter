fd9dfaed18ea6ec4bda38048a2a7acdf4562efab
awarbler <awoodfor@byui.edu>
2021-11-06 13:27:30 -0500

    added comments

--- batter/game/handle_collisions_action.py
+++ batter/game/handle_collisions_action.py
@@ -27,26 +27,24 @@ class HandleCollisionsAction(Action):
 
 
     def ball_brick_collision(self, cast):
+        """The responsibility of this class of objects is to update 
+        the game state when actors collide specifically ball to brick collision
+        Arg: 
+            bricks cast[bricks]
+            ball cast[ball]
+    """
         bricks = cast["brick"]
-
         ball = cast["ball"][0]# bball brick collision get ball position equal brick get position
         for brick in bricks:
             if ball.get_position().equals(brick.get_position()):
                 cast["brick"].remove(brick)
                 velocity = ball.get_velocity()
                 ball.set_velocity(velocity.reverse_y())
-         
-        
-                #ball.reverse() #ball.set_velocity(ball.get_veloctiy.reveser())  
-    # def paddle_boundaries (self, cast):
-    #     paddle = cast["paddle"]
-    #     pass
-    # Needs to get the position of the paddle.
-    # Compare paddle to 0.
-    # less then zero set paddle start back to zero
-    # If the paddle became more than max set back to max
 
     def ball_paddle_collision(self,cast):
+        """The responsibility of this class of objects is to update 
+        the game state when actors collide specifically ball to paddle collision 
+    """
 
         paddle = cast["paddle"][0]
         ball = cast["ball"][0]# bball brick collision get ball position equal brick get position
@@ -56,18 +54,16 @@ class HandleCollisionsAction(Action):
         ball_x_position = ball.get_position().get_x()
         ball_y_position = ball.get_position().get_y()
         
-        if (ball_y_position == paddle_y_position - 1 and ball_x_position >= paddle_x_position and ball_x_position <= paddle_x_position + len(paddle.get_text())):
+        if (ball_y_position == paddle_y_position +2 
+            and ball_x_position >= paddle_x_position 
+            and ball_x_position <= paddle_x_position + len(paddle.get_text())):
             velocity = ball.get_velocity()
             ball.set_velocity(velocity.reverse_y())
-        
- 
         pass
-        # Needs to loop through the length of the paddle and compare its position with the position of the ball.
-        # If the ball postition is equal to any portion of the paddle, bounce.
-        # need to return 
-
-
     def ball_wall_collision(self, cast):
+        """The responsibility of this class of objects is to update 
+        the game state when actors collide specifically ball to wall collision 
+    """
         ball = cast["ball"][0]
         position = ball.get_position()
         ball_x = position.get_x()
@@ -75,38 +71,20 @@ class HandleCollisionsAction(Action):
             velocity = ball.get_velocity()
             ball.set_velocity(velocity.reverse_x())
 
-
-        
-        # Needs to find the position of the ball.
-        # Then grab the y value of the ball.
-        # The check to see if y equals 0 + 1
-        # if yes, bounce the ball.
-        # Find the x value of ball.
-        # if x equal 0 or Max_X bounce.
-
-    
     def ball_floor_collision(self,cast):
+        """The responsibility of this class of objects is to update 
+        the game state when actors collide specifically ball to floor collision 
+    """
         ball = cast["ball"][0]
         position = ball.get_position()
         ball_y = position.get_y()
         if ball_y >= constants.MAX_Y +2: # - 1 to test but +2 to run game
-            #sys.exit()
-            velocity = ball.get_velocity()
-            ball.set_velocity(velocity.reverse_y())
-        # balPos = cast["ball"].get_position()
-        # if balPos.get_x() == constants.MAX_X or balPos.get_x() == 0 or balPos.get_y() == constants.MAX_Y:
-        #     pass
-
-        # if balPos.get_y() == 0:
-        #     quit()
-        # pass
-        # Needs to find the position of the ball.
-        # Then grab the y value of the ball.
-        # The check to see if y equals of MAX_Y
-        # Exit the system
-        pass
+            sys.exit()
     
     def ball_ceiling_collision(self, cast):
+        """The responsibility of this class of objects is to update 
+        the game state when actors collide specifically ball to ceiling collision 
+    """
 
         ball = cast["ball"][0]
         position = ball.get_position()
@@ -116,6 +94,9 @@ class HandleCollisionsAction(Action):
             ball.set_velocity(velocity.reverse_y())
 
     def paddle_boundaries(self,cast):
+        """The responsibility of this class of objects is to update 
+        the game state when actors collide specifically paddle to wall
+    """
         paddle = cast["paddle"][0]
         position = paddle.get_position()
         paddle_x = position.get_x()
